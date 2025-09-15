d14a1646e6ad635e5cf9ba5dee21c64161f49807
wr1 <8971152+wr1@users.noreply.github.com>
2025-09-15 23:27:04 +0200

    Import AFStep and LoftStep in api __init__.py

--- src/b3_geo/api/__init__.py
+++ src/b3_geo/api/__init__.py
@@ -1,7 +1,11 @@
 from .af import process_af
 from .loft import process_loft
+from .af_step import AFStep
+from .loft_step import LoftStep
 
 __all__ = [
     "process_af",
     "process_loft",
+    "AFStep",
+    "LoftStep",
 ]
