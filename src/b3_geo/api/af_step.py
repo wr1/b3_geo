0ca1c35332f573ddcca87530b8a59bafd1068e0f
wr1 <8971152+wr1@users.noreply.github.com>
2025-09-15 23:54:14 +0200

    Change _save_state to save_state in AFStep run method

--- src/b3_geo/api/af_step.py
+++ src/b3_geo/api/af_step.py
@@ -9,6 +9,16 @@ class AFStep(Statesman):
     dependent_sections = ["airfoils", "geometry"]
     output_files = ["b3_geo/airfoils.png", "b3_geo/airfoils.npz"]
 
+    def run(self, force=False):
+        self.logger.info("Starting run check.")
+        if force or self.needs_run():
+            self.logger.info("Step needs to run. Executing...")
+            self._execute()
+            self.logger.info("Execution completed.")
+            self.save_state()
+        else:
+            self.logger.info("Step does not need to run.")
+
     def _execute(self):
         from .af import process_af
 
