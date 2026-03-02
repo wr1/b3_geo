e257ec99b460eaa1b723f32d881808199efe4d22
wr1 <8971152+wr1@users.noreply.github.com>
2026-03-02 11:30:26 +0100

    Auto commit new files

--- src/b3_geo/api/loft_step.py
+++ src/b3_geo/api/loft_step.py
@@ -7,7 +7,7 @@ class LoftStep(Statesman):
     """Step for processing loft with statesman dependency management."""
 
     workdir_key = "workdir"
-    dependent_sections = ["geometry", "airfoils"]
+    dependent_sections = ["geometry", "airfoils", "mesh", "mesh3d"]
 
     def __init__(self, config_path, output_file=None, plot=True):
         super().__init__(config_path)
